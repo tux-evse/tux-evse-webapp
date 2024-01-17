d6488e7f848ea5d6d26b0f62b012f6a3e23cb06c
Sylvain MOCARD <sylvain.mocard@iot.bzh>
2024-01-17 11:38:54 +0100

    images C

--- evse_script/gen_tux_evse.sh
+++ evse_script/gen_tux_evse.sh
@@ -4,7 +4,7 @@ rm -f *.c
 cd ../../lv_img_conv/
 
 for img in $(ls ../tux-evse-webapp/src/assets/images); do
-    lv_img_conv.js ../tux-evse-webapp/src/assets/images/${img} -f -c CF_TRUE_COLOR_ALPHA
+    ./lv_img_conv.js ../tux-evse-webapp/src/assets/images/${img} -f -c CF_TRUE_COLOR_ALPHA
 done
 
 mv *.c ../tux-evse-webapp/rust_assets
@@ -12,7 +12,6 @@ mv *.c ../tux-evse-webapp/rust_assets
 cd -
 
 ./gen_tux_evse.py
-
 
 
 
